bbf4ccd25a4da844104e975b9a31cd643b7e1fe4
Marc Cartwright <uwvark27@gmail.com>
2020-08-23 14:24:33 -0400

    Changed PORT info

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ require('dotenv').config();
 console.log(process.env);
 
 const app = express();
-app.listen(3000, () => console.log('Weather! Listening at Port 3000'));
+const port = process.env.PORT || 3000;
+//app.listen(3000, () => console.log('Weather! Listening at Port 3000'));
+app.listen(port, () => {
+    console.log(`Weather! Listening at ${port}`);
+});
 app.use(express.static('public'));
 app.use(express.json({limit: '1mb'}));
 
